0f04b060a0249630b02ecac4275a45881771b051
ima-ocon <ima_ocon@yahoo.com>
2016-11-14 00:54:43 +0800

    Added demo service and controller for current user

--- app/assets/javascripts/angular/controllers/student_controller.js
+++ app/assets/javascripts/angular/controllers/student_controller.js
@@ -1,4 +1,12 @@
-Lrn.controller('StudentController', ['$scope',
-  function($scope) {
+Lrn.controller('StudentController', ['$scope', 'DemoService',
+  function($scope, DemoService) {
 
-}])
+    $scope.user_data = DemoService.dummyUserData()[0];
+    $scope.full_name = $scope.user_data.first_name + " " + $scope.user_data.last_name;
+
+    var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+      "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"
+    ];
+    $scope.month_joined = monthNames[$scope.user_data.date_joined.getMonth()];
+    $scope.year_joined = $scope.user_data.date_joined.getFullYear();
+}]);
