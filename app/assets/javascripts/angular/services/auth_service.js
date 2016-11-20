3c9ebcf01fd6ab2488fbb88af41a37b43972dd66
Life Christian <lifelofranco@gmail.com>
2016-11-20 16:00:19 +0800

    added 'ngProgress'

--- app/assets/javascripts/angular/services/auth_service.js
+++ app/assets/javascripts/angular/services/auth_service.js
@@ -1,29 +1,25 @@
 Lrn.service('AuthService', ["$rootScope", "$q", "$http", "$window", "$cookies", "$state", "ngDialog",
   function($rootScope, $q, $http, $window, $cookies, $state, ngDialog){
 
-  this.subscribe = function(data) {
-
-    var d = $q.defer();
-    $http({
-      method: 'POST',
-      url: '/subscribe',
-      data: data
-    }).success(function(data){
-      d.resolve(data);
+  var setUser = function(data) {
+    $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+    $http.post('https://lrn-api.herokuapp.com/api/v1/users/login', $.param(data))
+    .success(function(data){
+      $cookies.put('token', data.token);
+      $window.location.reload();
     });
 
-    return d.promise;
   }
 
   this.register = function(data) {
       $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
       $http.post('https://lrn-api.herokuapp.com/api/v1/users/register', $.param(data))
       .success(function(data){
-        $window.location.reload();
-
       });
+      setUser(data);
 
   };
+
 
   this.login = function(data) {
       $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
