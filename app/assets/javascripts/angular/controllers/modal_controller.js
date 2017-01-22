81c643da64e1f03b3a46907166925fbcfac8ca1c
Leo <leolopelofranco@gmail.com>
2017-01-22 22:09:53 +0800

    fixed popup validation

--- app/assets/javascripts/angular/controllers/modal_controller.js
+++ app/assets/javascripts/angular/controllers/modal_controller.js
@@ -2,4 +2,17 @@ Lrn.controller('ModalController', ['$scope', '$location', 'DemoService', 'ngDial
   function($scope, $location, DemoService, ngDialog, $state) {
 
     $scope.tickets = DemoService.ticket();
+    $scope.atLeastOne = true
+    $scope.selector = function(tickets) {
+      quantities = 0
+      _.each(tickets, function(e, i, l) {
+        quantities = quantities + parseInt(e.quantity)
+      });
+      if(quantities != 0) {
+        $scope.atLeastOne = false
+      }
+      else {
+        $scope.atLeastOne = true
+      }
+    }
 }]);
